45da0b9f0524e3a75e342b313e7840475a454bc4
vboxuser <vboxuser@SamemuDlaSiebie.myguest.virtualbox.org>
2023-08-07 12:00:26 +0200

    added three raw buttons_delete_add_book

--- conference_room/conf_app/views.py
+++ conference_room/conf_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.template.response import TemplateResponse
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.views import View
 from .models import Room
@@ -8,15 +10,40 @@ class DefaultPage(View):
     def get(self, request, *args, **kwargs):
         return TemplateResponse(request, 'base_html.html', context={})
 
+class AfterAddingRoomView(View):
+    def get(self, request, *args, **kwargs):
+        return TemplateResponse(request, 'after_adding_room.html', {})
+
 class AddingRooms(View):
     def get(self, request, *args, **kwargs):
-        return TemplateResponse(request, 'add_room.html', {})
+        return render(request, 'add_room.html')
+
+    def post(self, request):
+        name = request.POST.get("name")
+        capacity = request.POST.get("capacity", 0)
+        capacity = int(capacity) if capacity else 0
+        projector = request.POST.get("myCheckbox") == "on"
+        if not name:
+            return render(request, 'no_name.html')
+        if capacity <= 0:
+            return render(request, 'no_capacity.html')
+        if Room.objects.filter(name=name).first():
+            return render(request, 'room_already_exists.html')
+        Room.objects.create(name=name, capacity=capacity, projector=projector)
+        return redirect("added/")
 
 def list_of_rooms(request):
     rooms = Room.objects.all()
+    if not rooms:
+        message = ""
     return render(request, 'listed_rooms.html', {'rooms': rooms})
 
 def searching_by_id(request, room_id):
     rooms = get_object_or_404(Room, id=room_id)
     return render(request, 'room_detail.html', {'rooms': rooms})
+
+class DeleteRoomView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'listed_rooms.html')
+
 
--- conference_room/conference_room/urls.py
+++ conference_room/conference_room/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('room/', conf_views.DefaultPage.as_view()),
     path('room/list/', conf_views.list_of_rooms, name='list_of_rooms'),
     path('room/search/<int:room_id>/', conf_views.searching_by_id),
+    path('room/new/added/', conf_views.AfterAddingRoomView.as_view()),
+    path('room/new/', conf_views.AddingRooms.as_view()),
+    path('room/delete/<int:room_id>/', conf_views.DeleteRoomView.as_view())
 ]
